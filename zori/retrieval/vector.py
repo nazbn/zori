70d353bfc2ae0be8c9ee7ad06ff735f0b9e3c7b2
unknown <bagherinejad.n93@gmail.com>
2026-04-20 21:25:52 +0200

    refactor: switch to langchain-chroma from langchain-community

--- zori/retrieval/vector.py
+++ zori/retrieval/vector.py
@@ -1,4 +1,4 @@
-from langchain_community.vectorstores import Chroma
+from langchain_chroma import Chroma
 from langchain_core.embeddings import Embeddings
 
 from zori.ingestion.pdf import TextChunk
